ea065376861273f0697ab62158c9d0d8c060a04c
John Strunk <jstrunk@redhat.com>
2018-07-11 16:17:54 -0400

    Add flag to require all checks and add to Travis
    Adds --require-all flag to the pre-commit linter that will cause the
    script to fail if one of the checkers can't be found. This is added to
    ensure Travis fails if something goes wrong with installing prereqs as
    opposed to silently skipping the checks.
    
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 # vim: set ts=4 sw=4 et :
 
+# Usage: pre-commit.sh [--require-all]
+#   --require-all  Fail instead of warn if a checker is not found
+
 set -e
 
 # Run checks from root of the repo
@@ -17,11 +20,18 @@ function run_check() {
     if [ -x "$(command -v "$exe")" ]; then
         find . -regextype egrep -iregex "$regex" -print0 | \
             xargs -0rt -n1 "$exe" "$@"
-    else
+    elif [ "$all_required" -eq 0 ]; then
         echo "Warning: $exe not found... skipping some tests."
+    else
+        echo "FAILED: All checks required, but $exe not found!"
+        exit 1
     fi
 }
 
+all_required=0
+if [ "x$1" == "x--require-all" ]; then
+    all_required=1
+fi
 
 # Install via: gem install asciidoctor
 run_check '.*\.adoc' asciidoctor -o /dev/null -v --failure-level WARN
