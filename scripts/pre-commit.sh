6ec16c19a8a296263d2b70057041f5478b2634e3
John Strunk <jstrunk@redhat.com>
2018-07-11 16:01:36 -0400

    Add comments to pre-commit script
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -7,6 +7,7 @@ set -e
 scriptdir="$(dirname "$(realpath "$0")")"
 cd "$scriptdir/.."
 
+# run_check <file_regex> <checker_exe> [optional args to checker...]
 function run_check() {
     regex="$1"
     shift
@@ -21,9 +22,19 @@ function run_check() {
     fi
 }
 
+
+# Install via: gem install asciidoctor
 run_check '.*\.adoc' asciidoctor -o /dev/null -v --failure-level WARN
+
+# markdownlint: https://github.com/markdownlint/markdownlint
+# https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md
+# Install via: gem install mdl
 run_check '.*\.md' mdl
+
+# Install via: dnf install shellcheck
 run_check '.*\.(ba)?sh' shellcheck
+
+# Install via: pip install yamllint
 run_check '.*\.ya?ml' yamllint -s
 
 echo "ALL OK."
